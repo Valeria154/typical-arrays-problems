01ed48df6436b6aae492fe01de76401b4dab5a8e
Valeria154 <valeria.kazakevich@gmail.com>
2021-03-14 21:29:59 +0300

    feat: implement index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (typeof array === 'undefined' || array.length == 0) {
+        return 0;
+    }
+    return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array === 'undefined' || array.length == 0) {
+        return 0;
+    }
+    return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof array === 'undefined' || array.length == 0) {
+        return 0;
+    }
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    return sum / array.length;
 }
